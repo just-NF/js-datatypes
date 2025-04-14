35a0b1e2c59846eec970b664f8c2ae35d8639820
just-NF <ajokedi.aane@gmail.com>
2025-04-14 20:58:28 +0100

    datatypes and method

---
+++ datatypes.java
@@ -0,0 +1,146 @@
+public class datatypes {
+
+    public static void main(String[] args){
+  // -128 to 128 = a byte
+       byte Mybyte = 12;
+ 
+  // -32,768 t0 32,767
+       short MyShort = 1234;
+
+  // -2,147,483,648 to 2,147,483,647
+       int MyInteger = 2847
+
+  // -9,223,372,036,775,808 t0 9,223,372,036,775,807
+       long MyLong = 21646161984;
+
+  // about 6 decimals
+       float MyFloat = 1.2f;
+
+  // about 15 decimals
+       double MyDouble = 12.154;
+
+  // true or false
+       boolean MyBoolean = true;
+       boolean MoreThan1000 = (MyInteger > 1000);
+
+  // one character
+       char MyChar = 'W';
+       String MyString = "Martin";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ method.java
@@ -0,0 +1,13 @@
+public class method {
+
+public static void main(String[] args){ 
+
+    martin();
+
+}
+ public static void martin(){
+     system.out.println(x:"Hi,my name is martin");
+ }
+
+
+}
